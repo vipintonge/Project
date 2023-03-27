ef566c4e3843f4ccd4283d6f17179bf1129f0567
vipintonge <128152915+vipintonge@users.noreply.github.com>
2023-03-27 11:14:11 +0530

    Java code1

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+package day4_nestedLoop_Strings;
+/*
+ *   ******
+ *   ******
+ *   ******
+ *   ******
+ *   ******
+ */
+public class Pattern1 {
+
+	public static void main(String[] args)
+	{
+		for (int i=1;i<=5;i++)//row
+		{
+			for(int j=1;j<=3;j++)//column
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
